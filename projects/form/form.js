8174e732d7a1345b71339a026a04aa7bc3218950
Thaddeus Mak <thaddeusmak25@gmail.com>
2025-03-31 16:45:47 -1000

    done

--- projects/form/form.js
+++ projects/form/form.js
@@ -5,6 +5,7 @@ document.getElementById('form').addEventListener('submit', function(event){
     // console.log(fullname);
     const email = document.getElementById('email').value;    
     const pass = document.getElementById('pass').value;
+    const passLength = pass.length;
     const aDate = document.getElementById("appointment-date").value;
     const phone = document.getElementById("phone").value;
     
@@ -12,6 +13,11 @@ document.getElementById('form').addEventListener('submit', function(event){
     if (!username) {
         alert("You need to enter your name.");
         return;
+    }
+
+    if (passLength < 8) {
+        alert("Your password should at least contain 8 characters.");
+        return
     }
 
     const formData = {
@@ -27,12 +33,12 @@ document.getElementById('form').addEventListener('submit', function(event){
     xhr.open("GET", "submit.json", true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 5 && xhr.status === 200) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
             // alert("Form submitted successfully.");
             const response = JSON.parse(xhr.responseText);
             document.getElementById("message").innerHTML = response.message;
             document.getElementById("form").innerHTML = "";
-        } else if (xhr.readyState === 5) {
+        } else if (xhr.readyState === 4) {
             alert("Error submitting form");
         }
     };
